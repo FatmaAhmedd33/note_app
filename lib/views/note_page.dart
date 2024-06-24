0e1b2c977c9137abfa519f284ee4fa82d31a688d
fatma <faatmaaahmedd12@gmail.com>
2024-06-24 18:28:02 +0300

    add showModalBottomSheet

--- lib/views/note_page.dart
+++ lib/views/note_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/add_note_buttom_sheet.dart';
 import '../widgets/notes_view_body.dart';
 
 class NotePage extends StatelessWidget {
@@ -9,10 +10,20 @@ class NotePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      floatingActionButton: IconButton(onPressed: (){}, icon:const Icon( Icons.add)),
-      body:  const NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: IconButton(
+          iconSize: 30,
+          style: const ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)),
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return const AddNoteButtomSheet();
+                });
+          },
+          icon: const Icon(Icons.add)),
+      body: const NotesViewBody(),
     );
   }
 }
-
