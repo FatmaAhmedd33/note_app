1245213c67da10ea32a191f27309a88a824ee30b
fatma <faatmaaahmedd12@gmail.com>
2024-06-24 18:37:54 +0300

    add border radius to buttom sheet

--- lib/views/note_page.dart
+++ lib/views/note_page.dart
@@ -17,9 +17,12 @@ class NotePage extends StatelessWidget {
               backgroundColor: MaterialStatePropertyAll(Colors.blueAccent)),
           onPressed: () {
             showModalBottomSheet(
+                // any change in showModalBottomSheet to appear untill close it and open again cause has now build method here
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
                 context: context,
                 builder: (context) {
-                  return const AddNoteButtomSheet();
+                  return const AddNoteButtomSheet(); // but in widget AddNoteButtomSheet any change will relaod it will appear cause it in a build method
                 });
           },
           icon: const Icon(Icons.add)),
