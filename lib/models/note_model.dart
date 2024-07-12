77f56b801353a5b44e5da0aa7992d0719e909910
fatma <faatmaaahmedd12@gmail.com>
2024-07-12 19:59:16 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NoteModel {
+  final String title, subTitle, data;
+  final Color color;
+
+  NoteModel({required this.title, required this.subTitle, required this.data, required this.color});
+  
+}
